5045d21532d1bc83abd32a79ad1964350f3f94df
Nacho <ignacio.moreno-frison@capgemini.com>
2023-02-28 12:19:53 +0100

    se añaden datos de envio de formulario

---
+++ datos.php
@@ -0,0 +1,21 @@
+<?php
+    $campo = $_POST['Campo'];
+    echo 'El campo es '. $campo;
+    $nombre = $_POST['Nombre'];
+    echo 'El nombre es '. $nombre;
+    $apellidos = $_POST['Apellidos'];
+    echo 'El apellido es '. $apellidos;
+    $direccion = $_POST['Direccion'];
+    echo 'La direccion es '. $direccion;
+    $cp = $_POST['Cp'];
+    echo 'El codigo postal es '. $cp;
+    $email = $_POST['Email'];
+    echo 'El correo es '. $email;
+    $telefono = $_POST['Telefono'];
+    echo 'El telefono es '. $telefono;
+    $msg = $_POST['Msg'];
+    echo 'El mensaje es '. $Msg;
+    $check = $_POST['Check'];
+    echo 'Informacion '. $Check;
+    
+?>
